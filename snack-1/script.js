7e3a3defe2a3323a574d71a9d56b967750e59350
Elisei <eliseivlas.002@gmail.com>
2025-01-21 15:36:54 +0100

    es 1 e 2

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+names.forEach((nomi)=> {
+    console.log(nomi);
+    
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
